62a6bff2c9427388b191808e345b09f981652b5a
ROTY Jeremy <rotyjeremy@gmail.com>
2024-05-15 22:54:31 +0200

    Update

--- components/layouts/texts.ts
+++ components/layouts/texts.ts
@@ -1,6 +1,12 @@
 class Texts{
-    p(content:string){
-        return `<p></p>`;
+    p(content:RichText="My content", align=['', 'center', 'right', 'left']){
+        return `<p class="${align}">${content}</p>`;
+    }
+    span(content:string){
+        return `<span>${content}</span>`;
+    }
+    article(content:string){
+        return `<article>${content}</article>`;
     }
 }
 module.exports = Texts;
